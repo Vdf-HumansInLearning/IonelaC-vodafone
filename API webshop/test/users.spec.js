c598e573787073d8baa5b274595f0ce890eb959a
Ionela Ciobanasu <92990532+ionelaciobanasu@users.noreply.github.com>
2021-12-14 15:46:18 +0200

    Update users.spec.js

--- API webshop/test/users.spec.js
+++ API webshop/test/users.spec.js
@@ -1,11 +1,9 @@
 var chai = require('chai');
-let chaiHttp = require("chai-http");
 var assert = chai.assert;
 var expect = chai.expect;
 var should = chai.should();
 let users = require("../server");
 
-chai.use(chaiHttp);
 
 
 describe("Get user", function() {
